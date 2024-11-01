71bacd30cded5d61760846ea04506849b8e83144
juliaedyck <juliaedyck@gmail.com>
2024-11-01 16:50:27 +0100

    november

--- trance/src/components/GlobalStyles.js
+++ trance/src/components/GlobalStyles.js
@@ -6,7 +6,7 @@ export default createGlobalStyle`
 :root {
  --font-heading: "Sonsie One";
 	--font-body:"Poppins";
-    --background-color: #671ddf;
+    --background-color: #3bc1b9;
 	@media (min-width: 768px) {
    
       }
